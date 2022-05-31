171c4f25effa9fe789f6681e796c2a453ba08f10
Alif Jian <alifjian834@gmail.com>
2022-05-31 17:26:45 +0700

    Update whit express

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const port = 3000;
+const app = express();
+
+const expressLayouts = require("express-ejs-layouts");
+// Gunakan EJS
+
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.use("/asset",express.static(__dirname + '/asset'));
+
+app.get("/", (req, res) => {
+    res.render("index",{dir: "",layout: "mainLayout.ejs"});
+});
+
+app.get("/view/updated",(req,res) => {
+    res.render("updated", {dir: "..",layout: "mainLayout.ejs"})
+})
+
+app.use("/", (req,res) => {
+    res.send("<h1>ERROR 404 Not Found</h1>")
+})
+app.listen(port, () => {
+    console.log("Running in http://localhost:" + port);
+});
