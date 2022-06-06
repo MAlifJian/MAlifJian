6f7c4300fe366d65ea77ae7185f4199140ecd156
Alif Jian <alifjian834@gmail.com>
2022-06-06 14:20:18 +0700

    Full Responsive

--- app.js
+++ app.js
@@ -10,19 +10,19 @@ app.use(expressLayouts);
 app.use("/asset",express.static(__dirname + '/asset'));
 
 app.get("/", (req, res) => {
-    res.render("index",{dir: "",layout: "mainLayout.ejs",title: "OtakuRead - FansTranslation Light Novel Bahasa Indonesia"});
+    res.render("index",{dir: "",layout: "mainLayout.ejs",title: "OtakuRead - FansTranslation Light Novel Bahasa Indonesia", page: "home"});
 });
 
 app.get("/view/updated",(req,res) => {
-    res.render("view/updated", {dir: "..",layout: "mainLayout.ejs", title: "Update Novel Terbaru - FansTranslation Light Novel Bahasa Indonesia"})
+    res.render("view/updated", {dir: "..",layout: "mainLayout.ejs", title: "Update Novel Terbaru - FansTranslation Light Novel Bahasa Indonesia", page: "updated"})
 })
 
 app.get("/view/hottest", (req,res) => {
-    res.render("view/hottest", {dir: "..",layout: "mainLayout.ejs", title: "Update Novel Terpanas - FansTranslation Light Novel Bahasa Indonesia"})
+    res.render("view/hottest", {dir: "..",layout: "mainLayout.ejs", title: "Update Novel Terpanas - FansTranslation Light Novel Bahasa Indonesia", page: "hottest"})
 })
 
 app.get("/view/favorite", (req,res) => {
-    res.render("view/favorite", {dir: "..",layout: "mainLayout.ejs", title: "Novel Favorite Saya - FansTranslation Light Novel Bahasa Indonesia"})
+    res.render("view/favorite", {dir: "..",layout: "mainLayout.ejs", title: "Novel Favorite Saya - FansTranslation Light Novel Bahasa Indonesia", page: "favorite"})
 })
 
 app.use("/", (req,res) => {
